b5906b8eb2b33a7af18790c9d64b08fc717c6fdb
nahkim <48798620+nahkim@users.noreply.github.com>
2022-11-28 15:30:20 +0900

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path("", views.base, name="base"),
     path("accounts/", include("allauth.urls")),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
