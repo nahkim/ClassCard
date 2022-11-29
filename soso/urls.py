f2d2d1f26adda6cedc048788c63f0c936d81d6c7
nahkim <nahkim@student.42seoul.kr>
2022-11-29 14:41:59 +0900

    FEAT : 소셜 로그인 구현
    - 네이버 소셜 로그인 구현 완료
    - requirements.txt freeze 완료

--- soso/urls.py
+++ soso/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'articles'
+app_name = 'soso'
 
 urlpatterns = [
     path("login/", views.login),
--- soso/views.py
+++ soso/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def login(request):
-    return render(request, "articles/login.html")
+    return render(request, "soso/login.html")
