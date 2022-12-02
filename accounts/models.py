b73d97675c800af9da7c311c81fef693c73701cb
nahkim <nahkim@student.42seoul.kr>
2022-12-02 17:19:42 +0900

    FEAT : 소셜 로그인 model custom 추가
    - 기존 소셜 로그인 모델에 필요한 필드 추가

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('U', 'User'),
+        ('A', 'Admin'),
+    ]
+
+    nickname = models.CharField(max_length=10)
+    role = models.CharField(max_length=1, choices=ROLE_CHOICES)
+    profile = models.ImageField(
+        upload_to='images'
+    )
+    follow = models.ManyToManyField(
+        "self", symmetrical=False, related_name="followers"
+    )
