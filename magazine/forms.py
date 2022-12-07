c3896c2dd98a928932006942ed3e498010d59fe7
jiyeong99 <1999wldud@gmail.com>
2022-12-07 13:55:51 +0900

    FIX: 매거진 썸네일 이미지 추가

--- magazine/forms.py
+++ magazine/forms.py
@@ -6,9 +6,12 @@ from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
 class MagazineForm(ModelForm):
     class Meta:
         model = Magazine
-        fields = ('title', 'sub_title', 'content','tag')
+        fields = ('title', 'sub_title', 'content','tag','image')
         widgets = {
             'content': SummernoteWidget(),
+        }
+        label={
+            'image':'썸네일 이미지',
         }
 
 class MagazineCommentForm(ModelForm):
