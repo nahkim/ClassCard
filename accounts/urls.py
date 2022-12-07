b0e88a529ac75e4f96b77a11b4400e6719fe1719
nahkim <nahkim@student.42seoul.kr>
2022-12-07 10:09:47 +0900

    FEAT : 팔로우 기능 추가
    - 팔로우 기능 추가
    - 임시 profile.html 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = 'accounts'
+app_name = "accounts"
 
 urlpatterns = [
-    # path("update/", views.update, name="update"),
-    # path("delete/", views.delete, name="delete"),
-    # path("follow/<username>/", views.follow, name="follow"),
-    # path("profile/<username>/", views.profile, name="profile"),
+    path("update/", views.update, name="update"),
+    path("delete/", views.delete, name="delete"),
+    path("follow/<username>/", views.follow, name="follow"),
+    path("profile/<username>/", views.profile, name="profile"),
 ]
