5bbc607ee20a94b95d97fe6089f3ff7dc893808e
jejoonlee <jejoonlee1996@gmail.com>
2022-12-09 17:31:40 +0900

    FEAT: 장바구니 추가 기능

--- card/urls.py
+++ card/urls.py
@@ -11,7 +11,14 @@ urlpatterns = [
     path('comment_update/<int:card_id>/<int:comment_pk>/', views.comment_update, name="comment_update"),
     path('cardcompanylist/',views.cardcompanylist,name='cardcompanylist'),
     path('cardcompany/<str:company>/',views.cardcompany,name='cardcompany'),
+
+    # 카드 카테고리 검색을 한 후에 나타나는 카드 목록 페이지
     path('card_list/', views.card_list, name="card_list"),
+
+    # 카드를 비교하기 위해서 비교하기 바구니에 넣고 / 비교하기 페이지
+    path('bookmark/<int:pk>/', views.bookmark, name="bookmark"),
     path('card_compare/', views.card_compare, name="card_compare"),
+
+    # 카드를 검색했을 떄 보이는 카드 목록 페이지
     path('search/card_list/', views.search_list, name="search_list"),
 ]
